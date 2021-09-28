d4467d166b79fb6c7a59a8a19e5147ec32e912ae
silvan12 <72661914+silvan12@users.noreply.github.com>
2021-09-28 19:07:24 +0200

    add jump behavior

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 10f;
-
-    private void Start()
-    {
-
-    }
-
+    public float jumpSpeed = 300f;
+    public Rigidbody rb;
     void Update()
     {
         float xMovement = Input.GetAxisRaw("Horizontal");
-        float yMovement = Input.GetAxis("Vertical"); // jump/duck
-
         transform.position += new Vector3(xMovement, 0, 0) * (Time.deltaTime * movementSpeed);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up * jumpSpeed);
+        }
+    }
+
+    // FixedUpdate should be used for physics 
+    private void FixedUpdate()
+    {
+        
     }
 }
