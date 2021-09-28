92dc8816fca9eb580a93bb4053eccdc2ef7941fb
silvan12 <72661914+silvan12@users.noreply.github.com>
2021-09-28 19:28:29 +0200

    add ground check

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 10f;
     public float jumpSpeed = 300f;
+    public float distanceToGround = 0.5f;
+    
     public Rigidbody rb;
+
     void Update()
     {
         float xMovement = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(xMovement, 0, 0) * (Time.deltaTime * movementSpeed);
-        if (Input.GetKeyDown(KeyCode.W))
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) && IsGrounded())
         {
             rb.AddForce(Vector3.up * jumpSpeed);
         }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distanceToGround);
+
     }
 }
