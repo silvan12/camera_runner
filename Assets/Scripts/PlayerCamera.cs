481cf3f76989045aa65c58f429f4edb2499196bf
silvan12 <72661914+silvan12@users.noreply.github.com>
2021-09-28 18:09:07 +0200

    add x movement, camera toggle

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Camera topCamera;
+    public Camera sideCamera;
+    void Start()
+    {
+        topCamera.enabled = true;
+        sideCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            topCamera.enabled = !topCamera.enabled;
+            sideCamera.enabled = !sideCamera.enabled;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float yMovement = Input.GetAxis("Vertical"); // jump/duck
+
+        transform.position += new Vector3(xMovement, 0, 0) * (Time.deltaTime * movementSpeed);
+    }
+}
